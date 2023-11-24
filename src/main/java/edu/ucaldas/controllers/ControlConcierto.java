6cbc33ef4214b0dd058db4af124ea8f50ddf342f
Juan Pablo Valencia <pibe742337@gmail.com>
2023-11-24 01:13:05 -0500

    ConciertoException implementado. Método registrarBoletos implementado en ControlConcierto

--- src/main/java/edu/ucaldas/controllers/ControlConcierto.java
+++ src/main/java/edu/ucaldas/controllers/ControlConcierto.java
@@ -4,8 +4,10 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import edu.ucaldas.model.Concierto;
+import edu.ucaldas.model.ConciertoException;
 
 public class ControlConcierto {
 
@@ -23,7 +25,29 @@ public class ControlConcierto {
         conciertos.add(nuevoConcierto);
     }
 
-    
+    public Concierto buscarConciertoPorNombre(String nombre) throws ConciertoException{
+        for (Concierto concierto : conciertos) {
+            if (concierto.getNombre().equals(nombre)) {
+                return concierto;
+            }
+        }
+        throw new ConciertoException("El concierto " + nombre + " no ha sido realizado");
+    }
+
+    public void registrarBoletos(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el nombre del concierto: ");
+        String nombreConcierto = scanner.nextLine();
+        Concierto concierto = buscarConciertoPorNombre(nombreConcierto);
+        if (concierto != null) {
+            System.out.println("Ingrese el número de boletas vendidas: ");
+            int numeroBoletos = scanner.nextInt();
+            scanner.nextLine();
+            concierto.setBoletasVendidas(numeroBoletos);
+        } else {
+            System.out.println("No se encontró el concierto");
+        }
+    }
 
 
 
