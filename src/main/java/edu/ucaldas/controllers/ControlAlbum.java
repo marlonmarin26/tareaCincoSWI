2456bcf791d1b29615f5badb8bb350aa205bba5a
marlonmarin26 <marlon.1701910281@ucaldas.edu.co>
2023-11-25 16:34:55 -0500

    Manejo de excepciones y validaciones para entrada de datos

--- src/main/java/edu/ucaldas/controllers/ControlAlbum.java
+++ src/main/java/edu/ucaldas/controllers/ControlAlbum.java
@@ -1,7 +1,9 @@
 package edu.ucaldas.controllers;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -25,13 +27,26 @@ public class ControlAlbum {
 
             System.out.println("Ingrese la fecha de lanzamiento del álbum (formato YYYY-MM-DD): ");
             String fechaString = scanner.nextLine();
-            LocalDate fechaLanzamiento = LocalDate.parse(fechaString);
 
-            Album album = new Album(nombreAlbum, fechaLanzamiento);
-            album.setCancionesAlbum(new ArrayList<>());
-            listaAlbumes.add(album);
+            try {
+                LocalDate fechaLanzamiento = LocalDate.parse(fechaString);
 
-            System.out.println("Álbum agregado exitosamente.");
+                // Verificar si el nombre del álbum ya existe
+                if (albumYaExiste(nombreAlbum)) {
+                    System.out.println("¡Error! El álbum con el nombre '" + nombreAlbum + "' ya existe.");
+                    continue; // Vuelve al inicio del bucle para ingresar un nombre único.
+                }
+
+                Album album = new Album(nombreAlbum, fechaLanzamiento);
+                album.setCancionesAlbum(new ArrayList<>());
+                listaAlbumes.add(album);
+
+                System.out.println("Álbum agregado exitosamente.");
+
+            } catch (DateTimeParseException e) {
+                System.out.println("Formato de fecha incorrecto. Por favor, use el formato YYYY-MM-DD.");
+                continue; // Vuelve al inicio del bucle para ingresar una fecha válida.
+            }
 
             // Preguntar si desea agregar más álbumes
             System.out.print("¿Desea agregar más álbumes? (Sí/No): ");
@@ -44,6 +59,22 @@ public class ControlAlbum {
         } while (deseaAgregarMasAlbumes);
 
         System.out.println("Operación de agregar álbumes completada.");
+    }
+
+    /**
+     * Verifica si un álbum con el nombre proporcionado ya existe en la lista de
+     * álbumes.
+     * 
+     * @param nombreAlbum
+     * @return
+     */
+    private boolean albumYaExiste(String nombreAlbum) {
+        for (Album album : listaAlbumes) {
+            if (album.getNombre().equalsIgnoreCase(nombreAlbum)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
@@ -60,54 +91,63 @@ public class ControlAlbum {
         for (int i = 0; i < listaAlbumes.size(); i++) {
             System.out.println((i + 1) + ". " + listaAlbumes.get(i).getNombre());
         }
-
+    
         // Solicitar al usuario que seleccione un álbum
-        int indiceAlbumSeleccionado;
+        int indiceAlbumSeleccionado = 0;
         Album albumSeleccionado = null;
         do {
-            System.out.print("Seleccione el número del álbum al que desea agregar canciones (o 0 para cancelar): ");
-            indiceAlbumSeleccionado = scanner.nextInt();
-
-            if (indiceAlbumSeleccionado >= 1 && indiceAlbumSeleccionado <= listaAlbumes.size()) {
-                albumSeleccionado = listaAlbumes.get(indiceAlbumSeleccionado - 1);
-            } else if (indiceAlbumSeleccionado != 0) {
-                System.out.println("Número de álbum no válido.");
+            try {
+                System.out.print("Seleccione el número del álbum al que desea agregar canciones (o 0 para cancelar): ");
+                indiceAlbumSeleccionado = Integer.parseInt(scanner.nextLine());
+    
+                if (indiceAlbumSeleccionado >= 1 && indiceAlbumSeleccionado <= listaAlbumes.size()) {
+                    albumSeleccionado = listaAlbumes.get(indiceAlbumSeleccionado - 1);
+                } else if (indiceAlbumSeleccionado != 0) {
+                    System.out.println("Número de álbum no válido. Por favor, seleccione un número válido.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Por favor, ingrese un número válido.");
             }
         } while (albumSeleccionado == null && indiceAlbumSeleccionado != 0);
-
+    
         if (albumSeleccionado != null) {
             // Mostrar la lista de canciones disponibles
             System.out.println("Lista de canciones disponibles:");
             for (int i = 0; i < listaCanciones.size(); i++) {
                 System.out.println((i + 1) + ". " + listaCanciones.get(i).getNombre());
             }
-
+    
             // Solicitar al usuario que seleccione canciones hasta que decida parar
-            int indiceCancionSeleccionada;
+            int indiceCancionSeleccionada = 0;
             do {
-                System.out.print("Seleccione el número de la canción que desea agregar al álbum (o 0 para terminar): ");
-                indiceCancionSeleccionada = scanner.nextInt();
-
-                if (indiceCancionSeleccionada >= 1 && indiceCancionSeleccionada <= listaCanciones.size()) {
-                    Cancion cancionSeleccionada = listaCanciones.get(indiceCancionSeleccionada - 1);
-
-                    // Verificar si la canción ya está en el álbum
-                    if (!albumSeleccionado.getCancionesAlbum().contains(cancionSeleccionada)) {
-                        albumSeleccionado.getCancionesAlbum().add(cancionSeleccionada);
-                        System.out.println("Canción agregada al álbum exitosamente.");
-                    } else {
-                        System.out.println("La canción ya está en el álbum.");
+                try {
+                    System.out.print("Seleccione el número de la canción que desea agregar al álbum (o 0 para terminar): ");
+                    indiceCancionSeleccionada = Integer.parseInt(scanner.nextLine());
+    
+                    if (indiceCancionSeleccionada >= 1 && indiceCancionSeleccionada <= listaCanciones.size()) {
+                        Cancion cancionSeleccionada = listaCanciones.get(indiceCancionSeleccionada - 1);
+    
+                        // Verificar si la canción ya está en el álbum
+                        if (!albumSeleccionado.getCancionesAlbum().contains(cancionSeleccionada)) {
+                            albumSeleccionado.getCancionesAlbum().add(cancionSeleccionada);
+                            System.out.println("Canción agregada al álbum exitosamente.");
+                        } else {
+                            System.out.println("La canción ya está en el álbum. Por favor, seleccione otra canción.");
+                        }
+                    } else if (indiceCancionSeleccionada != 0) {
+                        System.out.println("Número de canción no válido. Por favor, seleccione un número válido.");
                     }
-                } else if (indiceCancionSeleccionada != 0) {
-                    System.out.println("Número de canción no válido.");
+                } catch (NumberFormatException e) {
+                    System.out.println("Por favor, ingrese un número válido.");
                 }
             } while (indiceCancionSeleccionada != 0);
-
+    
             System.out.println("Operación de agregar canciones al álbum completada.");
         } else {
             System.out.println("Operación cancelada. No se ha seleccionado ningún álbum.");
         }
     }
+    
 
     /**
      * Consulta y muestra la información de un álbum dado su nombre.
@@ -117,47 +157,54 @@ public class ControlAlbum {
      * Si el álbum no se encuentra, se muestra un mensaje indicando la ausencia.
      */
     public void consultarAlbum() {
-        // Solicitar al usuario ingresar el nombre del álbum a consultar
-        System.out.print("Ingrese el nombre del álbum a consultar: ");
-        String nombreAlbum = scanner.nextLine();
-
-        // Bandera para indicar si se encontró el álbum
-        boolean albumEncontrado = false;
-
-        // Iterar sobre la lista de álbumes para buscar el álbum por nombre
-        for (Album album : listaAlbumes) {
-            // Comparar el nombre del álbum ignorando mayúsculas y minúsculas
-            if (album.getNombre().equalsIgnoreCase(nombreAlbum)) {
-                albumEncontrado = true;
-
-                // Mostrar información del álbum encontrado
-                System.out.println("Información del álbum:");
-                System.out.println("Nombre: " + album.getNombre());
-                System.out.println("Fecha de lanzamiento: " + album.getFechaLanzamiento());
-
-                // Obtener la lista de canciones del álbum
-                List<Cancion> canciones = album.getCancionesAlbum();
-
-                // Verificar si el álbum tiene canciones y mostrarlas
-                if (canciones != null && !canciones.isEmpty()) {
-                    System.out.println("Canciones:");
-                    for (Cancion cancion : canciones) {
-                        System.out.println("- " + cancion.getNombre() + " (" + cancion.getDuracion() + ")");
+        try {
+            // Solicitar al usuario ingresar el nombre del álbum a consultar
+            System.out.print("Ingrese el nombre del álbum a consultar: ");
+            String nombreAlbum = scanner.nextLine();
+    
+            // Bandera para indicar si se encontró el álbum
+            boolean albumEncontrado = false;
+    
+            // Iterar sobre la lista de álbumes para buscar el álbum por nombre
+            for (Album album : listaAlbumes) {
+                // Comparar el nombre del álbum ignorando mayúsculas y minúsculas
+                if (album.getNombre().equalsIgnoreCase(nombreAlbum)) {
+                    albumEncontrado = true;
+    
+                    // Mostrar información del álbum encontrado
+                    System.out.println("Información del álbum:");
+                    System.out.println("Nombre: " + album.getNombre());
+                    System.out.println("Fecha de lanzamiento: " + album.getFechaLanzamiento());
+    
+                    // Obtener la lista de canciones del álbum
+                    List<Cancion> canciones = album.getCancionesAlbum();
+    
+                    // Verificar si el álbum tiene canciones y mostrarlas
+                    if (canciones != null && !canciones.isEmpty()) {
+                        System.out.println("Canciones:");
+                        for (Cancion cancion : canciones) {
+                            System.out.println("- " + cancion.getNombre() + " (" + cancion.getDuracion() + ")");
+                        }
+                    } else {
+                        System.out.println("El álbum no tiene canciones.");
                     }
-                } else {
-                    System.out.println("El álbum no tiene canciones.");
+    
+                    // No es necesario seguir buscando, salir del bucle
+                    break;
                 }
-
-                // No es necesario seguir buscando, salir del bucle
-                break;
             }
-        }
-
-        // Mostrar mensaje si el álbum no se encontró
-        if (!albumEncontrado) {
-            System.out.println("No se encontró un álbum con el nombre proporcionado: " + nombreAlbum);
+    
+            // Mostrar mensaje si el álbum no se encontró
+            if (!albumEncontrado) {
+                System.out.println("No se encontró un álbum con el nombre proporcionado: " + nombreAlbum);
+            }
+    
+        } catch (InputMismatchException e) {
+            System.out.println("Entrada inválida. Por favor, ingrese un nombre de álbum válido.");
+            scanner.nextLine(); // Consumir la entrada no válida
         }
     }
+    
 
     /**
      * Muestra la lista de álbumes disponibles.
--- src/main/java/edu/ucaldas/controllers/ControlConcierto.java
+++ src/main/java/edu/ucaldas/controllers/ControlConcierto.java
@@ -2,17 +2,17 @@ package edu.ucaldas.controllers;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
-import edu.ucaldas.model.Album;
-import edu.ucaldas.model.Cancion;
-import edu.ucaldas.model.Concierto;
-import edu.ucaldas.model.ConciertoException;
+import edu.ucaldas.model.*;
 
 public class ControlConcierto {
 
+    private static Scanner scanner = new Scanner(System.in);
     private List<Concierto> conciertos;
 
     public ControlConcierto() {
@@ -24,33 +24,47 @@ public class ControlConcierto {
      * a la lista de conciertos.
      */
     public void programarConcierto() {
-        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.print("Ingrese el nombre del concierto: ");
+            String nombre = scanner.nextLine();
 
-        System.out.print("Ingrese el nombre del concierto: ");
-        String nombre = scanner.nextLine();
+            System.out.print("Ingrese la fecha del concierto (formato YYYY-MM-DD): ");
+            LocalDate fecha = LocalDate.parse(scanner.nextLine());
 
-        System.out.print("Ingrese la fecha del concierto (formato YYYY-MM-DD): ");
-        LocalDate fecha = LocalDate.parse(scanner.nextLine());
+            System.out.print("Ingrese el lugar del concierto: ");
+            String lugar = scanner.nextLine();
 
-        System.out.print("Ingrese el lugar del concierto: ");
-        String lugar = scanner.nextLine();
+            System.out.print("Ingrese la hora del concierto (formato HH:mm): ");
+            LocalTime hora = LocalTime.parse(scanner.nextLine());
 
-        System.out.print("Ingrese la hora del concierto (formato HH:mm): ");
-        LocalTime hora = LocalTime.parse(scanner.nextLine());
+            int capacidad;
+            do {
+                System.out.print("Ingrese la capacidad del concierto: ");
+                while (!scanner.hasNextInt()) {
+                    System.out.println("Por favor, ingrese un número válido.");
+                    scanner.next(); // Consumir la entrada no válida
+                }
+                capacidad = scanner.nextInt();
+                if (capacidad <= 0) {
+                    System.out.println("La capacidad debe ser un número positivo.");
+                }
+            } while (capacidad <= 0);
 
-        System.out.print("Ingrese la capacidad del concierto: ");
-        int capacidad = scanner.nextInt();
+            // Consumir la nueva línea después de leer el entero
+            scanner.nextLine();
 
-        // Consumir la nueva línea después de leer el entero
-        scanner.nextLine();
+            // Crear un nuevo concierto con la información proporcionada
+            Concierto nuevoConcierto = new Concierto(nombre, fecha, lugar, hora, capacidad, 0, new ArrayList<>());
 
-        // Crear un nuevo concierto con la información proporcionada
-        Concierto nuevoConcierto = new Concierto(nombre, fecha, lugar, hora, capacidad, 0, new ArrayList<>());
+            // Agregar el nuevo concierto a la lista de conciertos
+            conciertos.add(nuevoConcierto);
 
-        // Agregar el nuevo concierto a la lista de conciertos
-        conciertos.add(nuevoConcierto);
+            System.out.println("Concierto programado exitosamente.");
+        } catch (DateTimeParseException e) {
+            System.out.println("Error al analizar la fecha u hora. Asegúrese de ingresar el formato correcto.");
+        } finally {
 
-        System.out.println("Concierto programado exitosamente.");
+        }
     }
 
     /**
@@ -59,49 +73,68 @@ public class ControlConcierto {
      * agregarlas al concierto seleccionado.
      */
     public void agregarCancionesConcierto() {
-        Scanner scanner = new Scanner(System.in);
+        try {
+            Scanner scanner = new Scanner(System.in);
 
-        // Mostrar la lista de conciertos disponibles
+            // Mostrar la lista de conciertos disponibles
+            mostrarListaConciertos();
+
+            // Solicitar al usuario que seleccione un concierto
+            System.out.print("Seleccione el número del concierto al que desea agregar canciones (o 0 para salir): ");
+            int indiceConciertoSeleccionado = scanner.nextInt();
+
+            if (indiceConciertoSeleccionado >= 1 && indiceConciertoSeleccionado <= conciertos.size()) {
+                Concierto conciertoSeleccionado = conciertos.get(indiceConciertoSeleccionado - 1);
+
+                // Mostrar la lista de canciones disponibles en álbumes
+                List<Cancion> cancionesDisponibles = obtenerCancionesEnAlbumes();
+                System.out.println("Lista de canciones disponibles:");
+                mostrarListaCanciones(cancionesDisponibles);
+
+                // Solicitar al usuario que seleccione canciones hasta que decida parar
+                int indiceCancionSeleccionada;
+                do {
+                    System.out.print(
+                            "Seleccione el número de la canción que desea agregar al concierto (o 0 para terminar): ");
+                    indiceCancionSeleccionada = scanner.nextInt();
+
+                    if (indiceCancionSeleccionada >= 1 && indiceCancionSeleccionada <= cancionesDisponibles.size()) {
+                        Cancion cancionSeleccionada = cancionesDisponibles.get(indiceCancionSeleccionada - 1);
+
+                        // Agregar la canción directamente al concierto
+                        conciertoSeleccionado.getCancionesConcierto().add(cancionSeleccionada);
+                        System.out.println("Canción agregada al concierto exitosamente.");
+                    } else if (indiceCancionSeleccionada != 0) {
+                        System.out.println("Número de canción no válido.");
+                    }
+                } while (indiceCancionSeleccionada != 0);
+
+                System.out.println("Operación de agregar canciones al concierto completada.");
+            } else {
+                System.out.println("Número de concierto no válido.");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Entrada inválida. Por favor, ingrese un número.");
+            scanner.nextLine(); // Consumir la entrada no válida
+        }
+    }
+
+    /**
+     * Muestra la lista de conciertos disponibles.
+     */
+    private void mostrarListaConciertos() {
         System.out.println("Lista de conciertos disponibles:");
         for (int i = 0; i < conciertos.size(); i++) {
             System.out.println((i + 1) + ". " + conciertos.get(i).getNombre());
         }
+    }
 
-        // Solicitar al usuario que seleccione un concierto
-        System.out.print("Seleccione el número del concierto al que desea agregar canciones (o 0 para salir): ");
-        int indiceConciertoSeleccionado = scanner.nextInt();
-
-        if (indiceConciertoSeleccionado >= 1 && indiceConciertoSeleccionado <= conciertos.size()) {
-            Concierto conciertoSeleccionado = conciertos.get(indiceConciertoSeleccionado - 1);
-
-            // Mostrar la lista de canciones disponibles en álbumes
-            List<Cancion> cancionesDisponibles = obtenerCancionesEnAlbumes();
-            System.out.println("Lista de canciones disponibles:");
-            for (int i = 0; i < cancionesDisponibles.size(); i++) {
-                System.out.println((i + 1) + ". " + cancionesDisponibles.get(i).getNombre());
-            }
-
-            // Solicitar al usuario que seleccione canciones hasta que decida parar
-            int indiceCancionSeleccionada;
-            do {
-                System.out.print(
-                        "Seleccione el número de la canción que desea agregar al concierto (o 0 para terminar): ");
-                indiceCancionSeleccionada = scanner.nextInt();
-
-                if (indiceCancionSeleccionada >= 1 && indiceCancionSeleccionada <= cancionesDisponibles.size()) {
-                    Cancion cancionSeleccionada = cancionesDisponibles.get(indiceCancionSeleccionada - 1);
-
-                    // Agregar la canción directamente al concierto
-                    conciertoSeleccionado.getCancionesConcierto().add(cancionSeleccionada);
-                    System.out.println("Canción agregada al concierto exitosamente.");
-                } else if (indiceCancionSeleccionada != 0) {
-                    System.out.println("Número de canción no válido.");
-                }
-            } while (indiceCancionSeleccionada != 0);
-
-            System.out.println("Operación de agregar canciones al concierto completada.");
-        } else {
-            System.out.println("Número de concierto no válido.");
+    /**
+     * Muestra la lista de canciones disponibles.
+     */
+    private void mostrarListaCanciones(List<Cancion> canciones) {
+        for (int i = 0; i < canciones.size(); i++) {
+            System.out.println((i + 1) + ". " + canciones.get(i).getNombre());
         }
     }
 
@@ -125,6 +158,10 @@ public class ControlConcierto {
         return cancionesEnAlbumes;
     }
 
+    /**
+     * Permite al usuario seleccionar un concierto de la lista de conciertos
+     * disponibles y registrar el número de boletos vendidos para ese concierto.
+     */
     public void registrarBoletosVendidos() {
         Scanner scanner = new Scanner(System.in);
 
@@ -137,14 +174,24 @@ public class ControlConcierto {
             if (concierto != null) {
                 System.out.print("Ingrese el número de boletas vendidas: ");
                 int numeroBoletos = scanner.nextInt();
+
+                // Consumir la nueva línea después de leer el entero
+                scanner.nextLine();
+
                 concierto.setBoletasVendidas(numeroBoletos);
                 System.out.println("Boletos registrados exitosamente.");
             }
         } catch (ConciertoException e) {
             System.out.println(e.getMessage());
+        } catch (InputMismatchException e) {
+            System.out.println("Entrada inválida. Por favor, ingrese un número.");
+            scanner.nextLine(); // Consumir la entrada no válida
         }
     }
 
+    /**
+     * Busca un concierto por su nombre en la lista de conciertos.
+     */
     public Concierto buscarConciertoPorNombre(String nombre) {
         for (Concierto concierto : conciertos) {
             if (concierto.getNombre().equals(nombre)) {
